d4f7fe6abbcf00e903aa13e9e7cf9f024e782021
Lavanbradley <79238572+Lavanbradley@users.noreply.github.com>
2021-10-11 20:47:15 -0700

    .

--- React/apiexamples/src/pages/Home.js
+++ React/apiexamples/src/pages/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <div className="home">
      <h2 className="homeH2">This App is dedicated to show my ability to get data from external API's</h2> 
      <p className="homeH2p homeUnder">Inside the hamburger menus are my examples</p>
-     <p className="homeH2p">1) My first example is called News Search. I am using the New York Times API to display new article to the browser. I also have a search feature attached. Give it a try!</p>
+     <p className="homeH2p">1) My first example is called News Search. I am using the New York Times API to display new articles to the browser. I also have a search feature attached. Give it a try!</p>
      <a href="/newsSearch" className="homeLinks homeUnder">News Search</a>
      <p className="homeH2p">2) In my second example I am using Axios to fetch data from the Breaking Bad API. I am also displaying the response data to the browser using some neat CSS tricks. </p>
      <a href="breakingBad" className="homeLinks homeUnder">Breaking Bad</a>
